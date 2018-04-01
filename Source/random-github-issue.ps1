a00fc2dad71ba255aa6f1159d765ddf2eaf662ff
stho32 <stho32github@gmail.com>
2018-04-01 09:28:26 +0200

    The file now contains 2 advanced functions that encapsulate the functionality in a better way (#4)
    Fixes #1

--- Source/random-github-issue.ps1
+++ Source/random-github-issue.ps1
@@ -4,22 +4,39 @@
 
 #>
 
-# This is your username
-$user = "stho32"
-$organization = "Mahrvengers"
+function Get-GitHub-Repositories
+{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string] $user
+    )
 
-$repositories = @()
-
-$repositories += Invoke-RestMethod -Uri "https://api.github.com/users/$user/repos"
-$repositories += Invoke-RestMethod -Uri "https://api.github.com/users/$organization/repos"
-
-# Friendly output
-Write-Host ""
-Write-Host "Hey there, here are 5 proposals you probably could work on today:"
-Write-Host ""
-
-$repositories | Get-Random -Count 5 | % {
-    Write-Host "  -" $_.full_name "( open issues: " $_.open_issues ")"
-    Write-Host "    URL :" $_.html_url
-    Write-Host ""
+    $result = Invoke-RestMethod -Uri "https://api.github.com/users/$user/repos"
+    return $result
 }
+
+function Test-RandomGitHubIssue
+{
+    [CmdletBinding()]
+    Param()
+
+    $repositories = @()
+
+    $repositories += Get-GitHub-Repositories "stho32"
+    $repositories += Get-GitHub-Repositories "Mahrvengers"
+    
+    # Friendly output
+    Write-Host ""
+    Write-Host "Hey there, here are 5 proposals you probably could work on today:"
+    Write-Host ""
+
+    $repositories | Get-Random -Count 5 | % {
+        Write-Host "  -" $_.full_name "( open issues: " $_.open_issues ")"
+        Write-Host "    URL :" $_.html_url
+        Write-Host ""
+    }
+}
+
+# Get-GitHub-Repositories "stho32"
+# Test-RandomGitHubIssue
